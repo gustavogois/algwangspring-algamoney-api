614df98f1e0ec51d0c89be5892aaacd62fb00f81
Gustavo Gois <gustavo.gois@gmail.com>
2020-08-02 17:15:45 +0100

    Delete Pessoa

--- src/main/java/study/gois/algamoneyapi/exceptionhandler/AlgamoneyExceptionHandler.java
+++ src/main/java/study/gois/algamoneyapi/exceptionhandler/AlgamoneyExceptionHandler.java
@@ -3,6 +3,7 @@ package study.gois.algamoneyapi.exceptionhandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -36,9 +39,21 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatus status,
+                                                                  WebRequest request) {
         return handleExceptionInternal(ex, criarListaDeErros(ex.getBindingResult()), headers,
                 HttpStatus.BAD_REQUEST, request);
+    }
+
+    @ExceptionHandler({ EmptyResultDataAccessException.class })
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex, WebRequest request) {
+        String messageUsuario = messageSource.getMessage("recurso.nao-encontrado", null,
+                LocaleContextHolder.getLocale());
+        String messageDesenvolvedor = ex.toString();
+        List<Erro> erros = Arrays.asList(new Erro(messageUsuario, messageDesenvolvedor));
+        return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
 
     private List<Erro> criarListaDeErros(BindingResult bindingResult) {
